f7056b0128991b155d4c65368ce00c7ae413b889
Yunus Sevimli <yunus2796yns@gmail.com>
2024-11-19 17:15:15 +0300

    Fragment'ler oluşturuldu. "KullaniciFragment.kt" tasarımı yapıldı ve "nav_graph.xml" dosyası oluşturuldu.

---
+++ app/src/main/java/com/yunussevimli/photosharingwithfirebase/KullaniciFragment.kt
@@ -0,0 +1,44 @@
+package com.yunussevimli.photosharingwithfirebase
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yunussevimli.photosharingwithfirebase.databinding.FragmentKullaniciBinding
+
+class KullaniciFragment : Fragment() {
+    private var _binding: FragmentKullaniciBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentKullaniciBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.kayitButton.setOnClickListener { kayitOl(it) }
+        binding.girisButton.setOnClickListener { girisYap(it) }
+    }
+
+    fun kayitOl(view: View){
+
+    }
+
+    fun girisYap(view: View){
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

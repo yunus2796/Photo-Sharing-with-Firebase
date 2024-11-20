b2ac5c3193cae600c296d4ac57197773663f3632
Yunus Sevimli <yunus2796yns@gmail.com>
2024-11-20 23:39:32 +0300

    firestore veritabanına yazma işlemi tamamlandı.

--- app/src/main/java/com/yunussevimli/photosharingwithfirebase/YuklemeFragment.kt
+++ app/src/main/java/com/yunussevimli/photosharingwithfirebase/YuklemeFragment.kt
@@ -18,9 +18,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.navigation.Navigation
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
@@ -37,6 +41,7 @@ class YuklemeFragment : Fragment() {
 
     private lateinit var auth : FirebaseAuth
     private lateinit var storage : FirebaseStorage
+    private lateinit var db : FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +49,7 @@ class YuklemeFragment : Fragment() {
 
         auth = Firebase.auth
         storage = Firebase.storage
+        db = Firebase.firestore
     }
 
     private fun registerLauncher() { // ActivityResultLauncher nesneleri oluşturulur
@@ -113,8 +119,21 @@ class YuklemeFragment : Fragment() {
             gorselReference.putFile(secilenGorsel!!).addOnSuccessListener { uploadTask ->
                 //url'yi alma işlemi yapacağız.
                 gorselReference.downloadUrl.addOnSuccessListener { uri ->
-                    val downloadUrl = uri.toString()
-                    println(downloadUrl)
+                    if(auth.currentUser != null){
+                        val downloadUrl = uri.toString()
+                        val postMap = hashMapOf<String, Any>()
+                        postMap.put("downloadURL",downloadUrl)
+                        postMap.put("email",auth.currentUser!!.email.toString())
+                        postMap.put("comment",binding.commentText.text.toString())
+                        postMap.put("date",Timestamp.now())
+
+                        db.collection("Posts").add(postMap).addOnSuccessListener { documentReference ->
+                            val action = YuklemeFragmentDirections.actionYuklemeFragmentToFeedFragment()
+                            Navigation.findNavController(view).navigate(action)
+                        }.addOnFailureListener { exception ->
+                            Toast.makeText(requireContext(),exception.localizedMessage,Toast.LENGTH_LONG).show()
+                        }
+                    }
                 }
             }.addOnFailureListener { exception ->
                 Toast.makeText(requireContext(),exception.localizedMessage,Toast.LENGTH_LONG).show()
